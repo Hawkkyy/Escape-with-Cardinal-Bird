ec9bd206a494071a5cd1763bfde5005c472eb527
Hawkyy <tr0550431@gmail.com>
2026-08-20 08:23:12 +0800

    Tested "Hello World!"

---
+++ EscapeWithCardinalBird/src/main/java/com/mycompany/escapewithcardinalbird/EscapeWithCardinalBird.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.escapewithcardinalbird;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class EscapeWithCardinalBird {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
